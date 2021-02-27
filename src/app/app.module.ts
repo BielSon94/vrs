3ac22625a88b8748391fea14d27f7699b83125f7
BielSon <bielson94@gmail.com>
2021-02-27 14:13:33 +0100

    New features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CoreModule } from '@core/core.module';
-import { ReservationModule } from '@features/reservation/reservations.module';
 
 
 @NgModule({
@@ -18,9 +16,14 @@ import { ReservationModule } from '@features/reservation/reservations.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     FontAwesomeModule,
-    CoreModule
+    CoreModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: "pl-PL"
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
